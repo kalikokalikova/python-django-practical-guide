403552c6dd5b294ed60db8111b314a19d6ac3089
kalikokalikova <calico.seders@gmail.com>
2023-09-07 13:39:34 -0700

    first commit

---
+++ monthly_challenges/challenges/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+# url config
+urlpatterns = [
+    # takes 2 args (string of url, view function)
+    path("", views.index),
+    path("<int:month>", views.monthly_challenge_by_int),
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
+]
---
+++ monthly_challenges/challenges/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+challenge_dict = {
+    'january': 'do some january shit',
+    'february': 'eat some candy',
+    'march': 'come in like a lion',
+    'april': 'be a flower',
+    'may': 'be in love',
+    'june': 'wish for summer',
+    'july': 'go outside'
+}
+
+
+def index(request):
+    response = "<ul>"
+    months = list(challenge_dict.keys())
+    for month in months:
+        month_path = reverse("month-challenge", args=[month])
+        response += f'<li><a href="{month_path}">{month.capitalize()}</a></li>'
+    response += "</ul>"
+    print(response)
+    return (HttpResponse(response))
+
+
+def monthly_challenge_by_int(request, month):
+    months = list(challenge_dict.keys())
+    if month > len(months):
+        return HttpResponseNotFound("invalid month")
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = challenge_dict[month]
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
+    except:
+        return HttpResponseNotFound("Bro that's not a thing")
