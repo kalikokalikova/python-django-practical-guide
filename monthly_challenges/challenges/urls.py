4a0de4215304d40b10fc18fca5d51374080dd6ea
kalikokalikova <calico.seders@gmail.com>
2023-09-13 15:52:07 -0700

    All of section 5

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 # url config
 urlpatterns = [
-    # takes 2 args (string of url, view function)
+    # takes 3 args (string of url, view function, name of url)
     path("", views.index, name="index"),
     path("<int:month>", views.monthly_challenge_by_int),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
