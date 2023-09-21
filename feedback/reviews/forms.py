4369d089069ec3b0763cb3587a06c5ac6ec6093b
kalikokalikova <calico.seders@gmail.com>
2023-09-21 15:03:46 -0700

    Beginning of class based views

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,6 +1,25 @@
 from django import forms
+from .models import Review
 
-class ReviewForm(forms.Form):
-  user_name = forms.CharField(label="Your Name", max_length=100, error_messages={"required": "NO EMPTIES!", "max_length": "TOO LONG"})
-  review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
-  rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+# class ReviewForm(forms.Form):
+#   user_name = forms.CharField(label="Your Name", max_length=100, error_messages={"required": "NO EMPTIES!", "max_length": "TOO LONG"})
+#   review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
+#   rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+
+class ReviewForm(forms.ModelForm):
+  class Meta:
+    model = Review
+    # fields = '__all__' => render all fields
+    # exclude = ['owner_comment'] => don't render this field
+    fields = ['user_name', 'review_text', 'rating'] # or just list the fields you want
+    labels = {
+      "user_name": "Your Name",
+      "review_text": "Your Feedback",
+      "rating": "Your Rating"
+    }
+    error_messages = {
+      "user_name": {
+        "required": "NO EMPTIES!@!",
+        "max_length": "TOO LONG!!!"
+      }
+    }
